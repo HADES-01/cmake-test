c22b7901cb16b6650bb3eaa1fb0a108558b1e715
Aditya Vats <adityavats122017@gmail.com>
2023-10-28 17:00:12 +0530

    GLFW submodule test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include "Human.h"
+#include <GLFW/glfw3.h>
 
 int main()
 {
+  GLFWwindow *windows;
+  const char *description;
+  if (!glfwInit())
+  {
+    glfwGetError(&description);
+    printf("Error: %s\n", description);
+    exit(EXIT_FAILURE);
+  }
   Human *h = new Human(1, "Aditya");
   h->show_age();
   h->show_name();
   delete h;
+  windows = glfwCreateWindow(300, 300, "Multi-Window Example", NULL, NULL);
+  while (true)
+  {
+    glfwMakeContextCurrent(windows);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(windows);
+
+    if (glfwWindowShouldClose(windows) ||
+        glfwGetKey(windows, GLFW_KEY_ESCAPE))
+    {
+      glfwTerminate();
+      exit(EXIT_SUCCESS);
+    }
+
+    glfwWaitEvents();
+  }
+
   return 0;
 }
